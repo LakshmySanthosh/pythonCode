de9e74e488d10eb6ae88570a5cd1fb7713487991
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-02-25 23:30:40 +0530

    Add files via upload
    Python program to find duplicate elements in a given array

---
+++ duplicateElementsInArray.py
@@ -0,0 +1,26 @@
+# initializing arrays
+a = []
+unique = []
+duplicate = []
+flag = 0
+
+# taking array input
+n = int(input("Enter the number of elements of the array: "))
+print("Enter the elements")
+for i in range(0, n):
+    m = int(input())
+    a.append(m)
+print(f"The array is {a}")
+
+# finding duplicate elements
+for i in range(0, n):
+    if a[i] not in unique:
+        unique.append(a[i])
+    elif a[i] not in duplicate:
+        duplicate.append(a[i])
+
+# printing duplicate elements
+if not duplicate:
+    print("No duplicate elements!!!")
+else:
+    print(f"The duplicate elements are: {duplicate}")
