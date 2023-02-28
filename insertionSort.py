47a37fb2b4a517a3886485b70a87930273391116
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-02-28 23:04:11 +0530

    Add files via upload
    incomplete

--- insertionSort.py
+++ insertionSort.py
@@ -9,11 +9,14 @@ for i in range(0, n):
 
 # sorting using insertion sort
 for i in range(1, n):
-    j = i
-    while j > 0 & a[j-1] > a[j]:
-        a[j-1], a[j] = a[j], a[j-1]
+    key = a[i]
+    j = i-1
+    while j >= 0 & a[j] > key:
+        a[j+1] = a[j]
         j = j-1
+    a[j+1] = key
 print(f"The sorted array is {a}")
 ############################################
 ############################################
 ############################################
+
