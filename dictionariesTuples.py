5150ff4aa8d5a035adf8adf37ff831988a12e92a
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-02-28 23:04:32 +0530

    Add files via upload
    incomplete

---
+++ dictionariesTuples.py
@@ -0,0 +1,31 @@
+# Tuples are immutable lists.
+# Elements of a list can be modified, but elements in a tuple can only be accessed, not modified.
+
+# Elements in a tuple are given inside () and separated by commas
+tuple_1 = (1, 3, 5, 7, 8, 9, 10)
+
+# Tuple with one element always has a comma at the end
+tuple_2 = (45,)
+
+print(f"Tuple with multiple elements = {tuple_1}")
+print(f"Tuple with 1 element = {tuple_1}")
+
+# Dictionaries are made up of key: value pairs.
+# In Python, lists and tuples are organized and accessed based on position.
+# Dictionaries in Python are organized and accessed using keys and values.
+
+# Elements in a dictionary are given inside {} and separated by commas
+
+# dictionary using integer keys
+dict_1 = {1: "Arya", 2: "Zara", 3: "Ankit"}
+
+# dictionary using char keys
+dict_2 = {"a": "hey", "b": "How", "c": "are", "d": "you?"}
+
+# adding elements to a dictionary
+dict_3 = {}
+dict_3[0] = "zero"
+dict_3[1] = "one"
+################################################
+################################################
+################################################
