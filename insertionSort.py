7eba844bb476cb14f392b4ea5896546ee5d79135
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-03-01 11:09:56 +0530

    Add files via upload
    done

--- insertionSort.py
+++ insertionSort.py
@@ -4,19 +4,15 @@ a = []
 # taking array input
 n = int(input("Enter the number of elements in the array: "))
 for i in range(0, n):
-    m = int(input())
+    m = int(input(f"Enter element {i+1}: "))
     a.append(m)
 
 # sorting using insertion sort
 for i in range(1, n):
     key = a[i]
-    j = i-1
-    while j >= 0 & a[j] > key:
-        a[j+1] = a[j]
+    j = i
+    while j > 0 and a[j-1] > a[j]:
+        a[j-1], a[j] = a[j], a[j-1]
         j = j-1
-    a[j+1] = key
-print(f"The sorted array is {a}")
-############################################
-############################################
-############################################
 
+print(f"The array sorted using insertion sort is {a}")
