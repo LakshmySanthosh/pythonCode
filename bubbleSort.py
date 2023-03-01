e9c9ebde97f6380743147d2bd7f21f34a5a09f3e
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-03-01 11:10:21 +0530

    Add files via upload
    done

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,7 +4,7 @@ a = []
 # taking array input
 n = int(input("Enter the number of elements in the array: "))
 for i in range(0, n):
-    m = int(input())
+    m = int(input(f"Enter element {i+1}: "))
     a.append(m)
 
 # sorting using bubble sort
@@ -12,4 +12,4 @@ for i in range(0, n):
     for j in range(0, n-i-1):
         if a[j] > a[j+1]:
             a[j], a[j+1] = a[j+1], a[j]
-print(f"The sorted array is {a}")
+print(f"The array sorted using bubble sort is {a}")
