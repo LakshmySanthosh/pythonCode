53933fe7cc14f8da6219f129e5df3d06f1e81491
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-04-11 23:34:12 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+# Create some strings
+string1 = "Hello"
+string2 = "world!"
+print("The strings are: ")
+print(string1)
+print(string2, "\n")
+
+# Concatenate the strings
+
+string3 = string1 + " " + string2
+
+# Print the concatenated string
+print("The concatenated string is: " )
+print(string3, "\n")
+
+# Access a substring
+substring = string3[0:5]
+
+# Print the substring
+print("Substring of the above printed string from 0 to 5 is: ")
+print(substring)
